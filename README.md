4a048c20258ad4a081c55c59f270e2a08c92be2b
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-11 17:00:20 -0500

    tenth commit lmao

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ node.js server from youtube crash course (link in readme)
 
 https://www.youtube.com/watch?v=fBNz5xF-Kx4
 
-this has been updated
+this has been updated again
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const jack = new person("Jack", 30)
 const b = new person("Brandon", 25)
 const c = new person("Cal", 90)
 const d = new person("ddd", 1222)
-
+const e = new person("eric", 6)
 
