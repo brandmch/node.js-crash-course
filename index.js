3f1225755ff1a3dbfe9c155345c7c51d3e877976
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-11 16:29:06 -0500

    fifth commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const person = require('./person')
 
 const jack = new person("Jack", 30)
+const b = new person("Brandon", 25)
+const c = new person("Cal", 90)
+const d = new person("ddd", 1222)
 
-jack.greeting()
+
--- person.js
+++ person.js
@@ -2,10 +2,12 @@ class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+
+        console.log('Hello! My name is ' + this.name + ' and I am ' + this.age)
     }
 
     greeting() {
-        console.log('My name is ' + this.name + ' and I am ' + this.age)
+
     }
 }
 
