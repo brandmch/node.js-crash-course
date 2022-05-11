d98f81a198e3b1037add7db11ea901f7a75814f6
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-11 18:50:06 -0500

    forteenth commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Logger = require('./Logger')
+const fs = require('fs')
+const path = require('path')
 
 const logger = new Logger()
 
-logger.on('message', (data) => console.log('Called listener: ', data))
+logger.on('message', (data) => {
 
-logger.log('Hello WORLD')
+
+    console.log('Called listener: ', data)
+})
+
+for (let i = 0; i < 100; i++) {
+    logger.log(`${i}`)
+}
--- logger.js
+++ logger.js
@@ -1,10 +1,20 @@
 const EventEmitter = require('events')
 const uuid = require('uuid')
+const fs = require('fs')
+const path = require('path')
 
 class Logger extends EventEmitter {
     log(msg) {
+        const num = uuid.v4()
         // Call event
-        this.emit('message', { id: uuid.v4(), msg })
+        this.emit('message', { id: num, msg })
+
+        // Appends file
+        fs.appendFile(path.join('logged_logs.txt'), `Called listener: {${num}, ${msg}}`, err => {
+            if (err) throw err
+            console.log('File appended')
+        })
+
     }
 }
 
