31bece199bc0c7cff2d06155e8ed569b89ede340
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-12 10:28:04 -0500

    seventeenth commit

--- index.js
+++ index.js
@@ -5,29 +5,80 @@ const food = require('./data.json')
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/') {
+    // if (req.url === '/') {
 
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content)
-        })
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if (err) throw err
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content)
+    //     })
 
+    // }
+
+    // if (req.url === '/about') {
+
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if (err) throw err
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content)
+    //     })
+
+    // }
+    // if (req.url == '/data') {
+    //     res.writeHead(200, 'application/json')
+    //     res.end(JSON.stringify(food))
+    // }
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Extension of file
+    let extname = path.extname(filePath);
+    console.log(extname);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
 
-    if (req.url === '/about') {
+    // Read file
 
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content)
-        })
-
-    }
-    if (req.url == '/data') {
-        res.writeHead(200, 'application/json')
-        res.end(JSON.stringify(food))
-    }
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
 })
 
 const PORT = process.env.PORT || 5000;
